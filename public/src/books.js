9894224a6eb0f268dab436bd2c0ebc9b8e3c68ad
cldev66@gmail.com <144075525+CLDEV66@users.noreply.github.com>
2023-12-11 17:43:56 -0500

    passed all tests in books.js file

--- public/src/books.js
+++ public/src/books.js
@@ -1,9 +1,13 @@
 function findAuthorById(authors, id) {
   // YOUR SOLUTION HERE
+  let found = authors.find((authors) => authors.id === id);
+  return found;
 }
 
 function findBookById(books, id) {
   // YOUR SOLUTION HERE
+  let found = books.find((books) => books.id === id);
+  return found;
 }
 
 // NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
